5f88330694fdd7367024ce4dba8db93c4f1f8233
40224893 <cassiehassell@hotmail.com>
2024-11-13 14:45:06 +0000

    Re-added deleted files

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,54 +2,54 @@ package com.napier.sem;
 
 import java.sql.*;
 
-public class App
-{
+public class App {
     public static void main(String[] args) {
         App a = new App();
+        if (args.length == 1) {
+            a.connect("localhost:33060", 10000);
+        } else {
+            a.connect("db:3306", 10000);
+        }
 
-        a.connect();
 
         City ct = a.getCity(1);
         a.displayCity(ct);
 
         a.disconnect();
     }
+
     /**
+     * 11/11/24 modified Main to default to localhost
      * Connect to the MySQL database.
+     * 11/11/24 updated connect method to add location and delay parameters
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect(String location, int delay) {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        boolean shouldWait = false;
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
+            try {
+                if (shouldWait) {
+                    Thread.sleep(delay);
+                }
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println(String.format("jdbc:mysql://%s/world?useSSL=false", location));
+                con = DriverManager.getConnection(String.format("jdbc:mysql://%s/world?useSSL=false", location), "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+                shouldWait = true;
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -63,24 +63,18 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
 
-    public City getCity (int ID)
-    {
+    public City getCity(int ID) {
         try {
             Statement stmt = con.createStatement();
             String strSelect =
@@ -89,8 +83,7 @@ public class App
                             + "WHERE ID =" + ID;
             ResultSet rset = stmt.executeQuery(strSelect);
 
-            if(rset.next())
-            {
+            if (rset.next()) {
                 City ct = new City();
                 ct.ID = rset.getInt("ID");
                 ct.Name = rset.getString("Name");
@@ -98,8 +91,7 @@ public class App
                 ct.District = rset.getString("District");
                 ct.Population = rset.getInt("Population");
                 return ct;
-            }
-            else
+            } else
                 return null;
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -108,10 +100,8 @@ public class App
         }
     }
 
-    public void displayCity(City ct)
-    {
-        if (ct != null)
-        {
+    public void displayCity(City ct) {
+        if (ct != null) {
             System.out.println(
                     ct.ID + " "
                             + ct.Name + " "
